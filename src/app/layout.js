40198bce836ff91f6e1f51959dd9e089fa85433a
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2026-04-06 03:49:21 +0600

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,8 +14,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Hutchinson Partnership Portal",
-  description: "Secure login for the Hutchinson APAC Ltd. Partnership Portal",
+  title: "Templeton Partnership Portal",
+  description: "Secure login for the Templeton Trust Fund Ltd. Partnership Portal",
 };
 
 export default function RootLayout({ children }) {
