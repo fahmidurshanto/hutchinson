b817818f8b7c8a054362453f56c5421f41fe3686
developerpalakbasak <palakbasak8@gmail.com>
2026-04-16 02:45:59 +0600

    make structured for development and productionand solved qrcode verification problem with auto redirection for qr code

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,9 @@ const nextConfig = {
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'https://server.hutchinsonapac.com/api/v1/:path*',
-        // destination: 'http://localhost:4000/api/v1/:path*',
+        destination: process.env.NODE_ENV === 'development'
+          ? `${process.env.LOCAL_API_HOST}/:path*`
+          : `${process.env.API_HOST}/:path*`,
       },
     ];
   },
